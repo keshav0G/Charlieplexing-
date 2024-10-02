d01fa74cee3720856b97f977e477ecc0a7745c86
keshav0G <155824391+keshav0G@users.noreply.github.com>
2024-10-03 00:10:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+/*
+ * charlieplexing avr.c
+ *
+ * Created: 6/10/2024 3:54:55 PM
+ * Author : Hp
+ */ 
+#define F_CPU 20000000L
+#include <avr/io.h>
+#include <avr/delay.h>
+
+
+int wait = 200;
+
+int main(void)
+{
+    
+    while (1) 
+    {
+		DDRB = 0x06;
+		PORTB = 0x02;
+		_delay_ms(200);
+		PORTB = 0x04;
+		
+		_delay_ms(200);
+		
+		DDRB = 0x05;
+		PORTB = 0x01;
+		_delay_ms(200);
+		PORTB = 0x04;
+		
+		_delay_ms(200);
+		
+		DDRB = 0x03;
+		PORTB = 0x01;
+		_delay_ms(200);
+		PORTB = 0x02;
+		_delay_ms(200);
+    }
+}
+
+
